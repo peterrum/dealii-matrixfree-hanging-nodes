d59ebf7e7b30bac944ea5c982bc61e185db7db62
Peter Munch <peterrmuench@gmail.com>
2021-09-25 16:43:46 +0200

    Precomile up to degree 4

--- benchmark_01.cc
+++ benchmark_01.cc
@@ -1,26 +1,22 @@
 #include "benchmark_01.h"
 
-/**
- * mpirun -np 1 ./benchmark_01 annulus 8 8 1
- * mpirun -np 1 ./benchmark_01 quadrant 4 8 1
- * mpirun -np 1 ./benchmark_01 quadrant_flexible 4 8 1
- */
-int
-main(int argc, char **argv)
+template <unsigned int dim, unsigned int max_degree, unsigned int degree_ = 1>
+void
+run(const std::string  geometry_type,
+    const unsigned int min_n_refinements,
+    const unsigned int max_n_refinements,
+    const unsigned int degree,
+    const bool         print_details)
 {
-  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
-
-  const unsigned int dim                = 3;
-  const unsigned int degree_precompiled = 1;
-
-  const std::string geometry_type =
-    argc > 1 ? std::string(argv[1]) : "quadrant";
-  const unsigned int min_n_refinements = argc > 2 ? atoi(argv[2]) : 6;
-  const unsigned int max_n_refinements = argc > 3 ? atoi(argv[3]) : 6;
-  const unsigned int degree            = argc > 4 ? atoi(argv[4]) : 1;
-  const bool         print_details     = true;
-
-  AssertThrow(degree_precompiled == degree, ExcNotImplemented());
+  if (degree != degree_)
+    {
+      run<dim, max_degree, std::min(max_degree, degree_ + 1)>(geometry_type,
+                                                              min_n_refinements,
+                                                              max_n_refinements,
+                                                              degree,
+                                                              print_details);
+      return;
+    }
 
   ConvergenceTable table;
 
@@ -28,7 +24,7 @@ main(int argc, char **argv)
        n_refinements <= max_n_refinements;
        ++n_refinements)
     {
-      Test<dim, degree_precompiled> test(geometry_type, n_refinements);
+      Test<dim, max_degree> test(geometry_type, n_refinements);
 
       const auto info = test.get_info(print_details);
 
@@ -93,4 +89,30 @@ main(int argc, char **argv)
       table.write_text(std::cout);
       std::cout << std::endl;
     }
+}
+
+/**
+ * mpirun -np 1 ./benchmark_01 annulus 8 8 1
+ * mpirun -np 1 ./benchmark_01 quadrant 4 8 1
+ * mpirun -np 1 ./benchmark_01 quadrant_flexible 4 8 1
+ */
+int
+main(int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+
+  const unsigned int dim        = 3;
+  const unsigned int max_degree = 4;
+
+  const std::string geometry_type =
+    argc > 1 ? std::string(argv[1]) : "quadrant";
+  const unsigned int min_n_refinements = argc > 2 ? atoi(argv[2]) : 6;
+  const unsigned int max_n_refinements = argc > 3 ? atoi(argv[3]) : 6;
+  const unsigned int degree            = argc > 4 ? atoi(argv[4]) : 1;
+  const bool         print_details     = true;
+
+  AssertThrow(degree <= max_degree, ExcNotImplemented());
+
+  run<dim, max_degree>(
+    geometry_type, min_n_refinements, max_n_refinements, degree, print_details);
 }
--- benchmark_01_likwid.cc
+++ benchmark_01_likwid.cc
@@ -1,5 +1,30 @@
 #include "benchmark_01.h"
 
+template <unsigned int dim, unsigned int max_degree, unsigned int degree_ = 1>
+void
+run(const std::string  geometry_type,
+    const unsigned int n_refinements,
+    const unsigned int degree,
+    const bool         do_cg,
+    const bool         do_apply_constraints,
+    const bool         do_apply_quadrature_kernel)
+{
+  if (degree != degree_)
+    {
+      run<dim, max_degree, std::min(max_degree, degree_ + 1)>(
+        geometry_type,
+        n_refinements,
+        degree,
+        do_cg,
+        do_apply_constraints,
+        do_apply_quadrature_kernel);
+      return;
+    }
+
+  Test<dim, degree_> test(geometry_type, n_refinements);
+  test.run(do_cg, do_apply_constraints, do_apply_quadrature_kernel);
+}
+
 /**
  * likwid-mpirun -np 1 -f -g MEM -m -O ./benchmark_01_likwid annulus 7 1 0 0 0
  */
@@ -13,8 +38,8 @@ main(int argc, char **argv)
   LIKWID_MARKER_THREADINIT;
 #endif
 
-  const unsigned int dim                = 3;
-  const unsigned int degree_precompiled = 1;
+  const unsigned int dim        = 3;
+  const unsigned int max_degree = 4;
 
   const std::string geometry_type =
     argc > 1 ? std::string(argv[1]) : "quadrant";
@@ -24,10 +49,14 @@ main(int argc, char **argv)
   const bool         do_apply_constraints       = argc > 4 ? atoi(argv[4]) : 1;
   const bool         do_apply_quadrature_kernel = argc > 5 ? atoi(argv[5]) : 0;
 
-  AssertThrow(degree_precompiled == degree, ExcNotImplemented());
+  AssertThrow(degree <= max_degree, ExcNotImplemented());
 
-  Test<dim, degree_precompiled> test(geometry_type, n_refinements);
-  test.run(do_cg, do_apply_constraints, do_apply_quadrature_kernel);
+  run<dim, max_degree>(geometry_type,
+                       n_refinements,
+                       degree,
+                       do_cg,
+                       do_apply_constraints,
+                       do_apply_quadrature_kernel);
 
 #ifdef LIKWID_PERFMON
   LIKWID_MARKER_CLOSE;
