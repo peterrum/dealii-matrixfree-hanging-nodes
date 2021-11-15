db9839c3161d60192f7ce0bf5cd3400d48046552
Peter Munch <peterrmuench@gmail.com>
2021-11-15 18:13:09 +0100

    Fix template arguments

--- benchmark_03.h
+++ benchmark_03.h
@@ -15,87 +15,91 @@
 
 using namespace dealii;
 
-namespace dealii::GridGenerator
+namespace dealii
 {
-  template <int dim>
-  void
-  create_quadrant(Triangulation<dim> &tria, const unsigned int n_refinements)
+  namespace GridGenerator
   {
-    // according to the description in A FLEXIBLE, PARALLEL, ADAPTIVE
-    // GEOMETRIC MULTIGRID METHOD FOR FEM (Clevenger, Heister, Kanschat,
-    // Kronbichler): https://arxiv.org/pdf/1904.03317.pdf
+    template <int dim>
+    void
+    create_quadrant(Triangulation<dim> &tria, const unsigned int n_refinements)
+    {
+      // according to the description in A FLEXIBLE, PARALLEL, ADAPTIVE
+      // GEOMETRIC MULTIGRID METHOD FOR FEM (Clevenger, Heister, Kanschat,
+      // Kronbichler): https://arxiv.org/pdf/1904.03317.pdf
 
-    hyper_cube(tria, -1.0, +1.0);
+      hyper_cube(tria, -1.0, +1.0);
 
-    if (n_refinements == 0)
-      return;
+      if (n_refinements == 0)
+        return;
 
-    tria.refine_global(1);
+      tria.refine_global(1);
 
-    for (unsigned int i = 1; i < n_refinements; ++i)
-      {
-        for (auto cell : tria.active_cell_iterators())
-          if (cell->is_locally_owned())
-            {
-              bool flag = true;
-              for (int d = 0; d < dim; d++)
-                if (cell->center()[d] > 0.0)
-                  flag = false;
-              if (flag)
+      for (unsigned int i = 1; i < n_refinements; ++i)
+        {
+          for (auto cell : tria.active_cell_iterators())
+            if (cell->is_locally_owned())
+              {
+                bool flag = true;
+                for (int d = 0; d < dim; d++)
+                  if (cell->center()[d] > 0.0)
+                    flag = false;
+                if (flag)
+                  cell->set_refine_flag();
+              }
+          tria.execute_coarsening_and_refinement();
+        }
+
+      AssertDimension(tria.n_global_levels() - 1, n_refinements);
+    }
+
+
+
+    template <int dim>
+    void
+    create_annulus(Triangulation<dim> &tria, const unsigned int n_refinements)
+    {
+      // according to the description in A FLEXIBLE, PARALLEL, ADAPTIVE
+      // GEOMETRIC MULTIGRID METHOD FOR FEM (Clevenger, Heister, Kanschat,
+      // Kronbichler): https://arxiv.org/pdf/1904.03317.pdf
+
+      hyper_cube(tria, -1.0, +1.0);
+
+      if (n_refinements == 0)
+        return;
+
+      for (int i = 0; i < static_cast<int>(n_refinements) - 3; ++i)
+        tria.refine_global();
+
+      if (n_refinements >= 1)
+        {
+          for (auto cell : tria.active_cell_iterators())
+            if (cell->is_locally_owned())
+              if (cell->center().norm() < 0.55)
                 cell->set_refine_flag();
-            }
-        tria.execute_coarsening_and_refinement();
-      }
+          tria.execute_coarsening_and_refinement();
+        }
 
-    AssertDimension(tria.n_global_levels() - 1, n_refinements);
-  }
+      if (n_refinements >= 2)
+        {
+          for (auto cell : tria.active_cell_iterators())
+            if (cell->is_locally_owned())
+              if (0.3 <= cell->center().norm() && cell->center().norm() <= 0.43)
+                cell->set_refine_flag();
+          tria.execute_coarsening_and_refinement();
+        }
 
-
-
-  template <int dim>
-  void
-  create_annulus(Triangulation<dim> &tria, const unsigned int n_refinements)
-  {
-    // according to the description in A FLEXIBLE, PARALLEL, ADAPTIVE
-    // GEOMETRIC MULTIGRID METHOD FOR FEM (Clevenger, Heister, Kanschat,
-    // Kronbichler): https://arxiv.org/pdf/1904.03317.pdf
-
-    hyper_cube(tria, -1.0, +1.0);
-
-    if (n_refinements == 0)
-      return;
-
-    for (int i = 0; i < static_cast<int>(n_refinements) - 3; ++i)
-      tria.refine_global();
-
-    if (n_refinements >= 1)
-      {
-        for (auto cell : tria.active_cell_iterators())
-          if (cell->is_locally_owned())
-            if (cell->center().norm() < 0.55)
-              cell->set_refine_flag();
-        tria.execute_coarsening_and_refinement();
-      }
-
-    if (n_refinements >= 2)
-      {
-        for (auto cell : tria.active_cell_iterators())
-          if (cell->is_locally_owned())
-            if (0.3 <= cell->center().norm() && cell->center().norm() <= 0.43)
-              cell->set_refine_flag();
-        tria.execute_coarsening_and_refinement();
-      }
-
-    if (n_refinements >= 3)
-      {
-        for (auto cell : tria.active_cell_iterators())
-          if (cell->is_locally_owned())
-            if (0.335 <= cell->center().norm() && cell->center().norm() <= 0.39)
-              cell->set_refine_flag();
-        tria.execute_coarsening_and_refinement();
-      }
-  }
-} // namespace dealii::GridGenerator
+      if (n_refinements >= 3)
+        {
+          for (auto cell : tria.active_cell_iterators())
+            if (cell->is_locally_owned())
+              if (0.335 <= cell->center().norm() &&
+                  cell->center().norm() <= 0.39)
+                cell->set_refine_flag();
+          tria.execute_coarsening_and_refinement();
+        }
+    }
+  } // namespace GridGenerator
+} // namespace dealii
 
 
 
@@ -188,12 +192,11 @@ public:
   void
   vmult(VectorType &dst, const VectorType &src) const
   {
-    LaplaceOperatorLocal<dim, fe_degree> local_operator;
+    LaplaceOperatorLocal local_operator;
     matrix_free.cell_loop(local_operator, dst, src);
   }
 
 private:
-  template <int dim, int fe_degree>
   class LaplaceOperatorLocal
   {
   public:
